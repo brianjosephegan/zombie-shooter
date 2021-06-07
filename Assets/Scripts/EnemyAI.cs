0cd5ecb8b178906fdc55e786f1e751c19b931d8f
Brian Egan <brianjoseph.egan@gmail.com>
2021-06-06 22:33:01 -0700

    Sandbox Environment with Chasing Zombie
    Add Nature asset pack from Kenny.nl. Create sandbox environment to develop gameplay mechanics. Add Zombie prefab and set up basic AI script using NavMeshAgent.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform targetTransform;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, targetTransform.position);
+
+        if (isProvoked)
+        {
+            navMeshAgent.SetDestination(targetTransform.position);
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
