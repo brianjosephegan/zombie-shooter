16282bc26e551125361f765e59e90a67620cde3f
Brian Egan <brianjoseph.egan@gmail.com>
2021-06-06 23:05:55 -0700

    Fire with Muzzle Flash
    Create a new script to handle firing weapon. Currently showing a muzzle flash for now.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] GameObject muzzleFlash;
+    [SerializeField] float muzzleFlashTime = 0.05f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine("Fire");
+        }
+    }
+
+    private IEnumerator Fire()
+    {
+        muzzleFlash.SetActive(true);
+        yield return new WaitForSeconds(muzzleFlashTime);
+        muzzleFlash.SetActive(false);
+    }
+}
