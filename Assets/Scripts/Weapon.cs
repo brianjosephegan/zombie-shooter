1206861634097dde39f28f756a4e7fa8301a750d
Brian Egan <brianjoseph.egan@gmail.com>
2021-06-08 21:21:35 -0700

    Hit Effect
    Add hit effect where the bullet hits.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Camera fpCamera;
     [SerializeField] GameObject muzzleFlash;
+    [SerializeField] GameObject[] hitEffects;
     [SerializeField] float muzzleFlashTime = 0.05f;
+    [SerializeField] float range = 100f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            StartCoroutine("Fire");
+            StartCoroutine("PlayMuzzleFlash");
+            ProcessRaycast();
         }
     }
 
-    private IEnumerator Fire()
+    private void ProcessRaycast()
+    {
+        RaycastHit raycastHit;
+        if (Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out raycastHit, range))
+        {
+            PlayHitEffect(raycastHit);
+        }
+    }
+
+    private void PlayHitEffect(RaycastHit raycastHit)
+    {
+        var hitEffect = hitEffects[Random.Range(0, hitEffects.Length)];
+        var impact = Instantiate(hitEffect, raycastHit.point, Quaternion.LookRotation(raycastHit.normal));
+        Destroy(impact, 0.1f);
+    }
+
+    private IEnumerator PlayMuzzleFlash()
     {
         muzzleFlash.SetActive(true);
         yield return new WaitForSeconds(muzzleFlashTime);
