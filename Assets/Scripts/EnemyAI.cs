d73b24b0c5d21f6a0213d8e02e8198d34a7b1b19
Brian Egan <brianjoseph.egan@gmail.com>
2021-06-12 12:06:03 -0700

    Zombie Attack Animation
    Implement basic Zombie attack animation and include it in the state machine for the Zombie's animation controller.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform targetTransform;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 5f;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
@@ -41,7 +42,34 @@ public class EnemyAI : MonoBehaviour
 
     private void EngageTarget()
     {
+        FaceTarget();
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AttackTarget();
+        }
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (targetTransform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    private void ChaseTarget()
+    {
+        GetComponent<Animator>().SetBool("Attack", false);
         GetComponent<Animator>().SetTrigger("Walk");
         navMeshAgent.SetDestination(targetTransform.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log($"Zombie says, while eating {targetTransform.name}: \"On nom nom nom.\"");
+        GetComponent<Animator>().SetBool("Attack", true);
     }
 }
