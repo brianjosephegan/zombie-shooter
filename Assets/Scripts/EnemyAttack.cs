cd73e8eb0b8ca27b05d92a94fd73e8181285a69f
Brian Egan <brianjoseph.egan@gmail.com>
2021-06-12 12:29:44 -0700

    Player Health
    Implement Player Health and reduce that health via a method call in the Zombie attack animation.

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] float damage = 50f;
+
+    PlayerHealth playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        playerHealth.TakeDamage(damage);
+    }
+}
