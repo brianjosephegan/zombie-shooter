a26cbc214c70190aeb9d53180c2547d41ecb2b5e
Brian Egan <brianjoseph.egan@gmail.com>
2021-06-18 19:13:23 -0700

    Weapon Zoom
    Add script to be able to zoom in the camera and zoom out the camera when right mouse button is clicked. Attach this component to the Sniper Rifle.

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
+    bool zoomedInToggle = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsController = GetComponentInParent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedInToggle)
+            {
+                fpCamera.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+                zoomedInToggle = false;
+            }
+            else
+            {
+                fpCamera.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+                zoomedInToggle = true;
+            }
+        }
+    }
+}
